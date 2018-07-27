894cb7b72a9ad1171071065a939a0fb0888174be
ValentinDk <tarasenok2012@mail.ru>
2018-07-27 14:06:28 +0300

    Refactoring

--- components/Database.php
+++ components/Database.php
@@ -8,7 +8,11 @@ class Database
 		$paramsPath = ROOT.'/config/db_params.php';
 		$params = include($paramsPath);
 
-		$db = new \PDO("mysql:host={$params['host']}; dbname={$params['dbname']}", $params['user'], $params['password'], $params['options']);
+		$db = new \PDO("mysql:host={$params['host']};
+		                     dbname={$params['dbname']}",
+                             $params['user'],
+                             $params['password'],
+                             $params['options']);
 		$db -> exec("set names utf8");
 
 		return $db;
